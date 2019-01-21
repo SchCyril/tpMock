94192f541b9c306cd3ac0f9507b37edcd4621b1a
Cyril <schneidenbach.cyril@gmail.com>
2019-01-21 12:46:51 +0100

    1

--- src/main/java/fr/dta/Mock/HttpDataService.java
+++ src/main/java/fr/dta/Mock/HttpDataService.java
@@ -7,7 +7,7 @@ import org.apache.http.client.fluent.Request;
 
 
 public class HttpDataService {
-	public static String url = "http://jsonip.com";
+	private static String url = "http://jsonip.com";
 
 	public String getJsonIp() {
 		try {
